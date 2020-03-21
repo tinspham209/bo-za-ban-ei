94807d0723af7f8b6278d289eb1f8277ad0f2ca0
quay189 <34814999+quay189@users.noreply.github.com>
2020-03-21 21:21:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as knnClassifier from "@tensorflow-models/knn-classifier";
+import { Howl } from "howler";
+import { initNotifications, notify } from '@mycv/f8-notification';
+
+import "./App.css";
+import soundURL from "./assets/hey_sondn.mp3";
+
+var sound = new Howl({
+  src: [soundURL]
+});
+
+const NOT_TOUCH_LABEL = "not_touch";
+const TOUCHED_LABEL = "touched";
+const TRAINING_TIMES = 50;
+const TOUCH_CONFIDENCE = 0.8;
 
 function App() {
+  const video = useRef();
+  const mobilenetModule = useRef();
+  const classifier = useRef();
+  const canPlaySound = useRef(true);
+  const [touched, setTouched] = useState(false);
+
+  const init = async () => {
+    console.log("init");
+    await setupCamera();
+    console.log("cc");
+    mobilenetModule.current = await mobilenet.load();
+    classifier.current = knnClassifier.create();
+    console.log("setupDone");
+    console.log("ko cham tay len mat va bam vao train 1");
+
+    initNotifications({ cooldown: 3000 });
+  };
+
+  const setupCamera = () => {
+    return new Promise((resolve, reject) => {
+      navigator.getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+      if (navigator.getUserMedia) {
+        navigator.getUserMedia(
+          {
+            video: true
+          },
+          stream => {
+            video.current.srcObject = stream;
+            video.current.addEventListener("loadeddata", resolve);
+          },
+          error => reject(error)
+        );
+      } else {
+        reject();
+      }
+    });
+  };
+
+  const train = async label => {
+    console.log(`[${label}] Dang training...`);
+    for (let i = 0; i < TRAINING_TIMES; i++) {
+      console.log(`Progress ${parseInt(((i + 1) / TRAINING_TIMES) * 100)}%`);
+      await training(label);
+    }
+  };
+
+  const training = label => {
+    return new Promise(async resolve => {
+      const embedding = mobilenetModule.current.infer(video.current, true);
+      classifier.current.addExample(embedding, label);
+      await sleep(100);
+      resolve();
+    });
+  };
+
+  const run = async () => {
+    const embedding = mobilenetModule.current.infer(video.current, true);
+    const result = await classifier.current.predictClass(embedding);
+    if (
+      result.label === TOUCHED_LABEL &&
+      result.confidences[result.label] > TOUCH_CONFIDENCE
+    ) {
+      console.log("touched");
+      setTouched(true);
+      if(canPlaySound.current){
+        canPlaySound.current = false;
+        sound.play();
+
+      }
+      notify('BỎ TAY RA', { body: 'BẠN VỪA CHẠM TAY VÀO MẶT.' });
+    } else {
+      console.log("not touch");
+      setTouched(false);
+    }
+    await sleep(200);
+    run();
+  };
+
+  const sleep = (ms = 0) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
+  useEffect(() => {
+    init();
+    sound.on('end', function(){
+      canPlaySound.current = true;
+
+    });
+    return () => {};
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`main ${touched ? "touched" : ""}`}>
+      <video className="video" autoPlay ref={video} />
+      <div className="control">
+        <button className="btn" onClick={() => train(NOT_TOUCH_LABEL)}>
+          Train 1
+        </button>
+        <button className="btn" onClick={() => train(TOUCHED_LABEL)}>
+          Train 2
+        </button>
+        <button className="btn" onClick={() => run()}>
+          Run
+        </button>
+      </div>
     </div>
   );
 }
